10d8be74c2169cb3b7cca740d66c98306d6109b1
Bodybro <ivan.stereotekk@gmail.com>
2022-09-01 13:16:16 +0300

    wanted create axios:
    web interface works

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 
 from pathlib import Path
+
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import JSONResponse
 from starlette.templating import Jinja2Templates
 from fastapi import FastAPI,Request,Form
 
 
 app = FastAPI(title="INDEXER APP", openapi_url="/openapi.json")
 
-from fastapi.middleware.cors import CORSMiddleware
-
-
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:8000/path",
-    "http://127.0.0.1:8000/index",
-
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-)
+# from fastapi.middleware.cors import CORSMiddleware
+#
+#
+# origins = [
+#
+#     "*",
+#
+#
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["127.0.0.1:8000/path",'http://localhost:8080','127.0.0.1'],
+#     allow_credentials=False,
+# )
 
 
 #Base path and templates
@@ -40,14 +39,16 @@ HOME_DIR = Path.home()
 @app.get("/index", response_class=HTMLResponse)
 async def indexer(request: Request):
     desktop = HOME_DIR/"Desktop"
-    return TEMPLATES.TemplateResponse("index.html", {"request": request, "home_dir":HOME_DIR,"_list": HOME_DIR.iterdir()})
+    return TEMPLATES.TemplateResponse("index.html", {"request": request, "home_dir":HOME_DIR,"_list": desktop.iterdir()})
 
 @app.get("/{folder}",)
 async def list_files(folder:str):
     new_path = HOME_DIR/f"{folder}"
+    print(new_path)
     return {"List of files - ":new_path.iterdir(),"You wanted a list a ":new_path}
 
 @app.post("/path")
+@app.get("/path")
 async def get_from_directory(path: str):
     my_path = Path(path)
     try:
--- static/script.js
+++ static/script.js
@@ -5,26 +5,29 @@ console.log(emPty);
 
 function getTextFrom(text){
     alert(text);
-    let myData = emPty + text;
-    console.log(myData);
+    let secondURLpart = text;
+    let myURL = "http://127.0.0.1:8000/path?path="+secondURLpart.trim();
+    console.log(myURL.trim());
+    let xhr = new XMLHttpRequest();
+	  xhr.open("POST", myURL);
+	  xhr.send(JSON.stringify(text));
+	  xhr.onreadystatechange = function() {
+  if (xhr.readyState != 4) {
+    return
+  }
 
+  if (xhr.status === 200) {
+    console.log('result', xhr.responseText)
+    document.getElementById("result").innerHTML = "Files in directory :"+ xhr.responseText;
+  } else {
+    console.log('err', xhr.responseText)
+    document.getElementById("result").innerHTML = "Empty directory: "+ xhr.responseText;
+  }
+}
 }
 
-function makePost(myData){
-        axios.post("https://127.0.0.1:8000/path", {
-        'path': myData,
-    })
-    .then((response) => {
-      console.log(response);
-    });
-
-}
-
-
-
-
-//function showMessage() {
-//  let message = "Привет, я JavaScript!";
+//	  console.log(xhr.responseText+ " ------- Result");
+//	  document.getElementById("result").innerHTML = xhr.responseText + "++++";
 //
-//  alert( message );
-//}
+//	  }
+//      let resText = JSON.stringify(xhr.responseJSON)
